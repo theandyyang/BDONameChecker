541fc9da8996953becad923c5da45120c0241353
Andy Yang <tehandyyang@gmail.com>
2016-02-15 14:47:44 -0800

    Add python scripts to the repository

---
+++ BDOCharacterCheck.py
@@ -0,0 +1,45 @@
+import re
+from twill.commands import *
+
+fileName = raw_input('Enter file name: ');
+
+try:
+	with open(fileName) as f:
+		names = f.readlines();
+except:
+	print("File not found");
+	exit(0);
+
+for i in range(len(names)):
+	names[i] = names[i].strip('\n');
+
+go('https://www.blackdesertonline.com/account/login')
+email = raw_input('Enter Email: ');
+pw = raw_input('Enter Password: ');
+fv("1", "email", email);
+fv("1", "password", pw);
+
+submit();
+result = []
+
+z = open("result.txt", 'w');
+
+go("https://www.blackdesertonline.com/shop/preorder/namereservation/AJAXIsExistCharacterName.json?serverName=na&characterName=Test");
+n = show();
+a = re.search("USER__NOT_LOGIN", n);
+if(a != None):
+	z.write("Failed to login");
+	exit();
+
+for i in range(len(names)):
+	go('https://www.blackdesertonline.com/shop/preorder/namereservation/AJAXIsExistCharacterName.json?serverName=na&characterName=' + names[i])
+	s = show();
+	m = re.search('true', s);
+	if(m == None):
+		result.insert(i, "Character name \"" + names[i] + "\" is Available");
+	else:
+		result.insert(i, "Character name \"" + names[i] + "\" is Not Available");
+
+for k in result:
+	z.write("%s\n" % k);
+
